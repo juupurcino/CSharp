6c6ccd497665f948b7d54db924440700f7687c20
juupurcino <jdiaspurcino@gmail.com>
2025-01-23 17:21:44 -0300

    war

--- Front/obj/Debug/net9.0/Front.AssemblyInfo.cs
+++ Front/obj/Debug/net9.0/Front.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07782b589793d96084568fdb8444fc05b535330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff4b9601e3412d76bca6055a35592c2eafdbe8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ WAR/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Random random = new Random();
+
+int batalhas = 2;
+
+List<int> ataqueList = [];
+List<int> defesaList = [];
+List<int> jogadasList = [];
+int vitorias = 0;
+
+int Dado() => random.Next(6) + 1;
+
+for (int i = 0; i < batalhas; i++)
+{
+    int defesa = 500;
+    int ataque = 1_000;
+
+    int jogadas = 0;
+
+    while (true)
+    {
+
+        if (defesa <= 0 || ataque <= 1)
+            break;
+
+        ataqueList.Clear();
+        defesaList.Clear();
+        jogadasList.Clear();
+        jogadas = 0;
+
+        // while (jogadas < 3)
+        // {
+        //     ataqueList.Add(Dado());
+        //     defesaList.Add(Dado());
+        //     jogadas++;
+        // }
+
+        for (int j = 0; j < (ataque <= 4 ? ataque -1 : 3); j++)
+            ataqueList.Add(Dado());
+
+        for (int j = 0; j < (defesa <= 4 ? defesa -1 : 3); j++)
+            defesaList.Add(Dado());
+
+        var ataqueOrder = ataqueList.OrderByDescending(x => x).ToList();
+        var defesaOrder = defesaList.OrderByDescending(x => x).ToList();
+
+        for (int j = 0; j < ataqueOrder.Count(); j++)
+        {
+            int venceu = defesaOrder[j] >= ataqueOrder[j] ? 0 : 1;
+            jogadasList.Add(venceu);
+        }
+
+        int qtdDefesa = jogadasList.Count(x => x == 0);
+        int qtdAtaque = jogadasList.Count(x => x == 1);
+
+        Console.WriteLine(qtdAtaque);
+        Console.WriteLine(qtdDefesa);
+
+        ataque -= qtdAtaque;
+        defesa -= qtdDefesa;
+
+        Console.WriteLine($"Ataque: {ataque}");
+        Console.WriteLine($"Defesa: {defesa}");
+
+        // Console.WriteLine(ataque);
+        // Console.WriteLine(defesa);
+    }
+
+    if (ataque > defesa)
+    {
+        vitorias++;
+    }
+
+    Console.WriteLine(vitorias);
+}
+
+Console.WriteLine($"Vitorias: {(vitorias * 100) / (float)batalhas}");
